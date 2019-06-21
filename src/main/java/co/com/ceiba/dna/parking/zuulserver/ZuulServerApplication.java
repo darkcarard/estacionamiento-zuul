bfe5cf48328b5dec52699079328b92018ee447d9
Carlos Javier Ardila Bohorquez - Ceiba Software <carlos.ardila@ceiba.com.co>
2019-06-21 09:20:21 -0500

    Ajustes oauth2 en zuul

--- src/main/java/co/com/ceiba/dna/parking/zuulserver/ZuulServerApplication.java
+++ src/main/java/co/com/ceiba/dna/parking/zuulserver/ZuulServerApplication.java
@@ -4,14 +4,65 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-public class ZuulServerApplication {
+@EnableWebSecurity
+@CrossOrigin("*")
+public class ZuulServerApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
 	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+	       return super.authenticationManagerBean();
+	}
+	
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+    	
+    	UserDetails user=User.builder().username("user").password(passwordEncoder().encode("secret")).
+    			roles("USER").build();
+    	UserDetails userAdmin=User.builder().username("admin").password(passwordEncoder().encode("secret")).
+    			roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user,userAdmin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new  BCryptPasswordEncoder();
+    }
+   
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        	.csrf().disable()
+            .authorizeRequests()
+            	.antMatchers("/","/index","/webpublico").permitAll()
+            	.antMatchers("/webprivado").authenticated()
+            	.antMatchers("/webadmin").hasRole("ADMIN").and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout() // Metodo get pues he desabilitado CSRF
+                .permitAll();
+    }
 }
